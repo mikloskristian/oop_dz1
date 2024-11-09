b433fcedd4d339bd696008d29040fd2aa353ea51
mikloskristian <kristian.miklos032@gmail.com>
2024-11-09 12:03:43 +0100

    Finished project.

---
+++ ClassLibrary/Episode.cs
@@ -0,0 +1,45 @@
+﻿namespace ClassLibrary
+{
+    public class Episode
+    {
+        private int viewerCount;
+        private double sumOfRatings;
+        private double highestRating;
+
+        public Episode()
+        {
+            this.viewerCount = 0;
+            this.sumOfRatings = 0.0;
+            this.highestRating = 0.0;
+        }
+
+        public Episode(int viewerCount, double sumOfRatings, double highestRating)
+        {
+            this.viewerCount = viewerCount;
+            this.sumOfRatings = sumOfRatings;
+            this.highestRating = highestRating;
+        }
+
+        public int GetViewerCount()
+        {
+            return this.viewerCount;
+        }
+
+        public double GetAverageScore()
+        {
+            return this.sumOfRatings / this.viewerCount;
+        }
+
+        public double GetMaxScore()
+        {
+            return this.highestRating;
+        }
+
+        public void AddView(float episodeRating)
+        {
+            viewerCount++;
+            sumOfRatings += episodeRating;
+            if(episodeRating > this.highestRating) { highestRating = episodeRating; }
+        }
+    }
+}
---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,40 @@
+﻿using ClassLibrary;
+
+namespace ConsoleApp
+{
+    internal class Program
+    {
+        static Random generator;
+
+        static void Main(string[] args)
+        {
+
+            Episode ep1, ep2;
+            ep1 = new Episode();
+            ep2 = new Episode(10, 64.39, 8.7);
+            int viewers = 10;
+            for (int i = 0; i < viewers; i++)
+            {
+                ep1.AddView(GenerateRandomScore());
+                Console.WriteLine(ep1.GetMaxScore());
+            }
+            if (ep1.GetAverageScore() > ep2.GetAverageScore())
+            {
+                Console.WriteLine("Episode 1 has a higher score.");
+                Console.WriteLine($"Viewers: {ep1.GetViewerCount()}");
+            }
+            else
+            {
+                Console.WriteLine("Episode 2 has a higher score.");
+                Console.WriteLine($"Viewers: {ep2.GetViewerCount()}");
+            }
+        }
+
+        public static float GenerateRandomScore()
+        {
+            generator = new Random();
+            double val = generator.NextDouble() * 10;
+            return (float)val;
+        }
+    }
+}
